8779245529765a9f32054bdc41633f64fe4e087d
TheDaidor2001 <danielcastillo1552001@hotmail.com>
2023-10-20 13:18:00 +0100

    componets od social icons added

---
+++ src/components/SocialItems.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+    link: {
+        type: String
+    },
+    text: {
+        type: String
+    }
+
+
+})
+</script>
+
+
+<template>
+    <li class="flex flex-col justify-center items-center text relative">
+        <a class="ancla" :href="link" target="_blank" :aria-label="text">
+            <slot />
+        </a>
+        <p class="border border-secondary-color rounded-xl px-2 py-1 mt-3 absolute top top-10 text-white">{{ text }}</p>
+    </li>
+</template>
+
+<style scoped>
+.text>p {
+    opacity: 0;
+    transition: all .25s ease-in-out;
+}
+
+.text:hover p {
+    opacity: 1;
+    transform: scale(1.1);
+}
+</style>
+
---
+++ src/components/icons/index.js
@@ -0,0 +1,5 @@
+import Github from "./Github.vue";
+import Linkedin from "./Linkedin.vue";
+import Curriculum from "./Curriculum.vue";
+
+export { Linkedin, Github, Curriculum };
